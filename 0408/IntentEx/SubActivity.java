a6a7eb0f39f452b7218f1b2561813556f44016f5
MINKI_KIM <53805997+mk642@users.noreply.github.com>
2024-04-13 22:19:40 +0900

    Create SubActivity.java

---
+++ 0408/IntentEx/SubActivity.java
@@ -0,0 +1,31 @@
+package com.example.week06;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = editText.getText().toString();
+
+                Intent intent = new Intent();
+                intent.putExtra("message", message);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
